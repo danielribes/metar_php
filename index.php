9e806c6a62d520f4cae8b4c5de7793badab2f022
Daniel Ribes <daniel@danielribes.com>
2023-08-19 13:50:38 +0200

    Prova de actualització remota

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $metar = explode(' ', $rawmetar[1]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>METAR</title>
+    <title>METAR LELL</title>
 </head>
 <body>
 <h1>
